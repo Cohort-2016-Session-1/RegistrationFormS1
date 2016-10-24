2900a48dd6518a2d0e98676ac1104d9896b70ee2
Tevis Tumulty <Tevis.Tumulty@west-mec.org>
2016-10-24 15:29:29 -0700

    10/24 class work

--- js/register.js
+++ js/register.js
@@ -6,6 +6,43 @@
 
 function validateForm(e) {
     'use strict';
+
+    if (typeof e == 'undefined') {
+        //This is a browser window-generated event
+        e = window.event;
+    }
+
+    //Get form references:
+    var firstName = U.$('firstName');
+    var lastName;
+    var email;
+    var phone;
+    var city;
+    var state;
+    var zip;
+    var terms; //We'll populate these later
+
+    //error flag:
+    var error = false;
+
+    //Validate the first name using a
+    //regular expression:
+    if (/^[A-Z \.\-']{2,20}$/i.test(firstName.value)) {
+        // Everything between / and / is the expression
+        //Any letter A-Z (case insensitive) is valid
+        //Spaces, periods, and hyphens are valid
+        //name must be 2 - 20 characters long
+
+        //First name matches the expression
+        //and is valid
+        alert("Valid first name");
+
+    }
+    else {
+        alert("Invalid first name");
+        error = true;
+    }
+
     
 } // End of validateForm() function.
 
@@ -19,5 +56,16 @@ function toggleSubmit() {
 // Establish functionality on window load:
 window.onload = function() {
     'use strict';
+
+    //addEvent requires three pieces of info:
+    //1. The object to add the event listener to
+    //2. The event to listen for
+    //3. The function to call in response to
+    //  the event in #2
+    U.addEvent(
+        U.$('theForm'),
+        'submit',
+        validateForm);
+
 
 };
